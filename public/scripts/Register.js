a896cc89338b6b158879674debdc3061d4e7a2b2
Bartek <submission93@gmail.com>
2020-11-16 12:23:44 +0100

    delete local storage

--- public/scripts/Register.js
+++ public/scripts/Register.js
@@ -3,7 +3,7 @@ class Register {
 
         this.inputLogin = document.getElementById('login')
         this.inputPassword = document.getElementById('password')
-        this.users = []
+
 
     }
 
@@ -19,41 +19,7 @@ class Register {
             }
 
 
-            if (window.localStorage.getItem('users')) {
-                const existingData = JSON.parse(window.localStorage.getItem('users'));
-
-                for (let i = 0; i < existingData.length; i++) {
-                    if (existingData[i].login === userData.login) {
-                        if (document.querySelector('.red-alert')) {
-                            return;
-                        } else {
-                            this.alert("Taki użytkownik już jest członkiem Black Market");
-                            break;
-                        }
-                    } else {
-                        if (this.users.length === 0) {
-                            existingData.map((obj) => {
-                                return this.users.push(obj);
-                            })
-                            this.users.push(userData);
-                            localStorage.setItem('users', JSON.stringify(this.users))
-                            window.location.href = "index.html";
-                        } else if (this.users.length > 0) {
-                            this.users.push(userData);
-                            localStorage.setItem('users', JSON.stringify(this.users))
-                            console.log(this.users);
-                            window.location.href = "index.html";
-                        }
-                    }
-                }
-            } else {
-                //success when no users at all
-                this.users.push(userData);
-                localStorage.setItem('users', JSON.stringify(this.users))
-                window.location.href = "index.html";
-            }
         }
-
     }
     alert(text) {
         const par = document.createElement('p')
