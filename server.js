b3a503697b7f6b8e7fdd112cc2377f6a88dc4302
Bartek <submission93@gmail.com>
2020-11-17 18:17:21 +0100

    add register use function via mongodb

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const path = require('path');
+const MongoClient = require('mongodb').MongoClient;
 
 
+
+
+/* Database connection  */
+
+async function main(newUser) {
+
+    const uri = "mongodb+srv://bartek:qFaRliUrJ6ISD77V@cluster0.blizs.mongodb.net/<blackmarket>?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    try {
+        await client.connect();
+        await showCollections(client, newUser)
+    } catch (e) {
+        console.log(e);
+    }
+
+}
+
+
+async function showCollections(client, newUser) {
+
+    client.db('blackmarket').collection('users').find().toArray((err, content) => {
+        console.log(content);
+    });
+
+    client.db('blackmarket').collection('users').insertOne(newUser);
+
+
+}
+
+/* express functions */
 
 const app = express();
-
-
 app.use(express.json())
 app.use(express.urlencoded({
     extended: true
@@ -14,11 +46,8 @@ app.use(express.static('static'))
 
 app.post('/register', (req, res) => {
 
-    console.log('ok');
-    console.log(req.body);
-
+    main(req.body).catch(console.error);
     res.end();
-
 })
 
 
